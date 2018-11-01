11ff6291de7b10e119d29094bb398619d65e0489
feitingjun <449221252@qq.com>
2018-11-01 14:43:15 +0800

    no message

--- sqlconfig.js
+++ sqlconfig.js
@@ -1,6 +1,6 @@
 const config = {
     database: 'koa', // 使用哪个数据库
-    username: 'admin', // 用户名
+    username: 'root', // 用户名
     password: '4888982138fei', // 口令
     host: '127.0.0.1', // 主机名
     port: 3306 // 端口号，MySQL默认3306
